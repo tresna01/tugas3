bf5a74e72ba88eb1936770b29704a8156e265d5e
tresna01 <148548388+tresna01@users.noreply.github.com>
2024-02-28 09:55:02 +0700

    Add files via upload

---
+++ inputnilai.py
@@ -0,0 +1,6 @@
+nilai = int(input("MASUKAN NILAI ANDA :"))
+
+if nilai >= 75 :
+    print("SELAMAT ANDA DINYATAKAN LOLOS")
+else:
+    print("MAAF ANDA TIDAK DINYATAKAN LOLOS")
---
+++ konversinilai.py
@@ -0,0 +1,12 @@
+nilai = int(input("masukan nilai anda :"))
+
+if nilai <= 50 :
+    print("E")
+elif nilai <= 60 :
+    print("D")
+elif nilai <=70 :
+    print("C")
+elif nilai <=80 :
+    print("B")
+else:
+    print("A")
